3a03b94222693d7daceac37a19569e4aa2421a94
Roshni Mathew <125427804+Roshnimathew2706@users.noreply.github.com>
2023-11-14 10:11:02 +0530

    Create mainactivity.java

---
+++ cycle2/imageview/mainactivity.java
@@ -0,0 +1,31 @@
+{
+
+    private ImageView imageView1;
+    private ImageView imageView2;
+    private Button toggleButton;
+    private boolean isImage1Visible = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        toggleButton = findViewById(R.id.toggleButton);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isImage1Visible) {
+                    imageView1.setVisibility(View.GONE);
+                    imageView2.setVisibility(View.VISIBLE);
+                } else {
+                    imageView1.setVisibility(View.VISIBLE);
+                    imageView2.setVisibility(View.GONE);
+                }
+                isImage1Visible = !isImage1Visible;
+            }
+        });
+    }
+}
